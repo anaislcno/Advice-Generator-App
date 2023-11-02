520a73070ae6e0cdf25d3ae2fdc6b8aca23a4e67
An <anaislcno@gmail.com>
2023-11-02 15:34:30 +0100

    Initial commit w/ first card integration

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Attribution from "./components/Attribution/Attribution";
+import Card from "./components/Card/Card";
+
+function App() {
+  return (
+    <>
+      <Attribution />
+      <Card />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+import "./Card.css";
+
+function Card() {
+  return (
+    <>
+      <section className="card">
+        <h1 className="advice__nb">Advice #XX</h1>
+        <p className="advice">Description XX</p>
+      </section>
+    </>
+  );
+}
+
+export default Card;
