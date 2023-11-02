d56678f4e4d5c4672d8049a7bd1efb9b3a677067
An <anaislcno@gmail.com>
2023-11-02 18:51:04 +0100

    set media queries

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,9 +1,23 @@
 import { useState, useEffect } from "react";
 import "./Card.css";
 import Divider from "./../../assets/images/pattern-divider-desktop.svg";
+import MobileDivider from "./../../assets/images/pattern-divider-mobile.svg";
 
 const Card = () => {
   const [advice, setAdvice] = useState({ slip_id: 0, advice: "" });
+  const [width, setWidth] = useState(window.innerWidth > 767);
+
+  const updateMedia = () => {
+    setWidth(window.innerWidth > 767);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+
+    return () => {
+      window.removeEventListener("resize", updateMedia);
+    };
+  }, []);
 
   const fetchAdvice = () => {
     fetch("https://api.adviceslip.com/advice")
@@ -33,7 +47,11 @@ const Card = () => {
       <section className="card">
         <h1 className="advice__nb">Advice #{advice.slip_id}</h1>
         <p className="advice">"{advice.advice}"</p>
-        <img className="divider" src={Divider} alt="divider" />
+        {width ? (
+          <img className="divider" src={Divider} alt="divider" />
+        ) : (
+          <img className="divider" src={MobileDivider} alt="divider" />
+        )}
         <button onClick={fetchAdvice} className="dice__btn"></button>
       </section>
     </>
